6371143bed47628af62a1e5dac7b1c99b30fff59
danil <dddd@gmail.com>
2023-12-07 12:11:48 +0300

    задание 8

--- antonov227_07.12/MainWindow.xaml.cs
+++ antonov227_07.12/MainWindow.xaml.cs
@@ -28,51 +28,21 @@ namespace antonov227_07._12
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             int a = int.Parse(AA.Text);
-            string r = "";
-            switch (a / 100)
+            int b = int.Parse(BB.Text);
+
+            while (a != 0 && b != 0)
             {
-                case 1: r = "cто "; break;
-                case 2: r = "двести "; break;
-                case 3: r = "триста "; break;
-                case 4: r = "четыреста "; break;
-                case 5: r = "пятьсот "; break;
-                case 6: r = "шестьсот "; break;
-                case 7: r = "семьсот "; break;
-                case 8: r = "восемьсот "; break;
-                case 9: r = "девятьсот "; break;
+                if (a > b)
+                {
+                    a = a % b;
+                }
+                else
+                {
+                    b = b % a;
+                }
             }
-            switch (a % 100 / 10)
-            {
-                case 2: r += "двадцать "; break;
-                case 3: r += "тридцать "; break;
-                case 4: r += "сорок "; break;
-                case 5: r += "пятьдесят "; break;
-                case 6: r += "шестьдесят "; break;
-                case 7: r += "семьдесят "; break;
-                case 8: r += "восемьдесят "; break;
-                case 9: r += "девяносто "; break;
-            }
-            switch (a % 10)
-            {
-                case 1: r = "один"; break;
-                case 2: r += "два"; break;
-                case 3: r += "три"; break;
-                case 4: r += "четыре"; break;
-                case 5: r += "пять"; break;
-                case 6: r += "шесть"; break;
-                case 7: r += "семь"; break;
-                case 8: r += "восемь"; break;
-                case 9: r += "девять"; break;
-            }
-            MessageBox.Show($"Ответ: {r}");
 
-
-
-
-
-
-
-
+            MessageBox.Show($"Ответ: {a + b}");
         }
     }
 }
