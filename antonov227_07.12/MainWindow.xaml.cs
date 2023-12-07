2d8bb216b44c036e6ab77447e8ef3aa3c03c0cd6
danil <dddd@gmail.com>
2023-12-07 12:05:22 +0300

    задание 7

--- antonov227_07.12/MainWindow.xaml.cs
+++ antonov227_07.12/MainWindow.xaml.cs
@@ -27,17 +27,44 @@ namespace antonov227_07._12
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int n = int.Parse(AA.Text);
-            int last = 0;
-            string R = "";
-            while (n > 0)
+            int a = int.Parse(AA.Text);
+            string r = "";
+            switch (a / 100)
             {
-                last = n % 10;
-                n /= 10;
-                R += $"{last}";
+                case 1: r = "cто "; break;
+                case 2: r = "двести "; break;
+                case 3: r = "триста "; break;
+                case 4: r = "четыреста "; break;
+                case 5: r = "пятьсот "; break;
+                case 6: r = "шестьсот "; break;
+                case 7: r = "семьсот "; break;
+                case 8: r = "восемьсот "; break;
+                case 9: r = "девятьсот "; break;
             }
-            MessageBox.Show($"Ответ: {R}");
-
+            switch (a % 100 / 10)
+            {
+                case 2: r += "двадцать "; break;
+                case 3: r += "тридцать "; break;
+                case 4: r += "сорок "; break;
+                case 5: r += "пятьдесят "; break;
+                case 6: r += "шестьдесят "; break;
+                case 7: r += "семьдесят "; break;
+                case 8: r += "восемьдесят "; break;
+                case 9: r += "девяносто "; break;
+            }
+            switch (a % 10)
+            {
+                case 1: r = "один"; break;
+                case 2: r += "два"; break;
+                case 3: r += "три"; break;
+                case 4: r += "четыре"; break;
+                case 5: r += "пять"; break;
+                case 6: r += "шесть"; break;
+                case 7: r += "семь"; break;
+                case 8: r += "восемь"; break;
+                case 9: r += "девять"; break;
+            }
+            MessageBox.Show($"Ответ: {r}");
 
 
 
