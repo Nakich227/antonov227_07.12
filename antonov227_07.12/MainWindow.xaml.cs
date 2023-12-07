51355639737b89b329d66e8ff79a2bec49b4cf08
danil <dddd@gmail.com>
2023-12-07 10:40:26 +0300

    задание 3

--- antonov227_07.12/MainWindow.xaml.cs
+++ antonov227_07.12/MainWindow.xaml.cs
@@ -28,28 +28,48 @@ namespace antonov227_07._12
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             int a = Convert.ToInt32(AA.Text);
-            int b = Convert.ToInt32(BB.Text);
-            string s = "";
+            double b = Convert.ToInt32(BB.Text);
+            double z, x, c, v;
             switch (a)
             {
-                case 6: s = "шестёрка"; break;
-                case 7: s = "семерка"; break;
-                case 8: s = "восмерка"; break;
-                case 9: s = "девятка"; break;
-                case 10: s = "десятка"; break;
-                case 11: s = "валет"; break;
-                case 12: s = "дама"; break;
-                case 13: s = "король"; break;
-                case 14: s = "туз"; break;
+                case 1:
+                    z = b ;
+                    x = z * Math.Sqrt(3) / 6;
+                    c = 2 *x;
+                    v = z * z * Math.Sqrt(3) / 4;
+                    Console.WriteLine(x);
+                    Console.WriteLine(c);
+                    Console.WriteLine(v);
+                    break;
+                case 2:
+                    x = b;
+                    z = x * 6 / Math.Sqrt(3);
+                    c = 2 * x;
+                    v = a * a * Math.Sqrt(3) / 4;
+                    Console.WriteLine(z);
+                    Console.WriteLine(c);
+                    Console.WriteLine(v);
+                    break;
+                case 3:
+                    c = b;
+                    x = c / 2;
+                    z = x * 6 / Math.Sqrt(3);
+                    v = a * a * Math.Sqrt(3) / 4;
+                    Console.WriteLine(z);
+                    Console.WriteLine(x);
+                    Console.WriteLine(v);
+                    break;
+                case 4:
+                    v = b;
+                    z = Math.Sqrt(v * 4 / Math.Sqrt(3));
+                    x = a * Math.Sqrt(3) / 6;
+                    c = 2 * x;
+                    Console.WriteLine(x);
+                    Console.WriteLine(c);
+                    Console.WriteLine(z);
+                    break;
             }
-            switch (b)
-            {
-                case 1: s += " пики"; break;
-                case 2: s += "  крести"; break;
-                case 3: s += " буби"; break;
-                case 4: s += " черви"; break;
-            }
-            MessageBox.Show($"ответ: {s}") ;
+
 
 
         }
