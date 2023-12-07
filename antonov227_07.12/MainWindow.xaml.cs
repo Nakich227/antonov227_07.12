59c5bd8a8be69ee757e05c651f3aad5fe32dde5c
danil <dddd@gmail.com>
2023-12-07 11:42:43 +0300

    задание 4

--- antonov227_07.12/MainWindow.xaml.cs
+++ antonov227_07.12/MainWindow.xaml.cs
@@ -28,40 +28,22 @@ namespace antonov227_07._12
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             int n = Convert.ToInt32(AA.Text);
-            double x = Convert.ToInt32(BB.Text);
-            double a, r1, r2, s;
-
-            switch (n)
+            int k = Convert.ToInt32(BB.Text);
+            double q = 0;
+            while (n >= k)
             {
-                case 1:
-                    a = x;
-                    r1 = a * Math.Sqrt(3) / 6;
-                    r2 = 2 * r1;
-                    s = a * a * Math.Sqrt(3) / 4;
-                    MessageBox.Show($"ответ: r1 = {r1} r2 = {r2} s = {s}");
-                    break;
-                case 2:
-                    r1 = x;
-                    a = r1 * 6 / Math.Sqrt(3);
-                    r2 = 2 * r1;
-                    s = a * a * Math.Sqrt(3) / 4;
-                    MessageBox.Show($"ответ: a = {a} r2 = {r2} s = {s}");
-                    break;
-                case 3:
-                    r2 = x;
-                    r1 = r2 / 2;
-                    a = r1 * 6 / Math.Sqrt(3);
-                    s = a * a * Math.Sqrt(3) / 4;
-                    MessageBox.Show($"ответ: a = {a} r1 = {r1} s = {s}");
-                    break;
-                case 4:
-                    s = x;
-                    a = Math.Sqrt(s * 4 / Math.Sqrt(3));
-                    r1 = a * Math.Sqrt(3) / 6;
-                    r2 = 2 * r1;
-                    MessageBox.Show($"ответ: r1 = {r1} r2 = {r2} a = {a}");
-                    break;
+                n -= k;
+                q++;
             }
+
+            MessageBox.Show($"ответ: q{q} n{n}");
+
+
+
+
+
+
+
 
 
 
